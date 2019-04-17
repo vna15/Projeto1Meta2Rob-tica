f5a5ae4623081378be8207f4cec83f8c47f987f0
vna15 <30414490+vna15@users.noreply.github.com>
2019-04-17 15:49:09 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,219 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#define PI 3.1415926535
 
-#include "ufrn_al5d.h"
+double cinedireta1(base,ombro,cotovelo,punho)
 
-#define POS_INICIAL_GLOBAL "#0P1500#1P1500#2P1500#3P1500#4P1500"
-#define KEY_RIGHT 11
-#define KEY_LEFT 11
-#define INCREMENTAR 77
-#define REDUZIR 75
-
-
-int main()
 {
-    ufrn_header();
 
-	int serial_fd, tecla;
-	int base = 1500, ombro = 1500, cotovelo = 1500, punho = 1500, garra = 1500;
-	char *comando;
+double x, A1,A2,A3,A4;
 
+A1 = ((base-500)*0.09 + 180)*(PI/180);
 
-	printf("INICIALIZANDO...\n\n");
+A2 = ((ombro-500)*0.09)*(PI/180);
 
-	serial_fd = abrir_porta();
+A3 = ((cotovelo-500)*0.09)*(PI/180);
 
-	if(serial_fd == -1)
-	{
-		printf("Erro ao abrir a porta serial /dev/ttyS0\nAbortando o programa...");
-		return -1;
-	}
-	else
-	{
-		printf("Porta serial /dev/ttyS0 aberta com sucesso\n");
+A4 = ((punho-500)*0.09 - 90)*(PI/180);
 
-		if(configurar_porta(serial_fd) == -1)
-		{
-		    printf("Erro inicializando a porta\n");
-		    close(serial_fd);
-		    return -1;
-		}
-
-		comando = (char*) malloc(sizeof(char)*BUFSIZE);
-
-		printf("\nCOLOCANDO O BRAÇO ROBÓTICO NA POSIÇÃO INICIAL\n");
-
-		sprintf(comando,"%s",POS_INICIAL);
-		enviar_comando(comando,serial_fd);
-		memset(comando, 0, BUFSIZE);
-
-		printf("Use as setas para movimentar cada servo do braço robótico\nApós terminar a movimentação aperte a tecla Z\n");
-		printf("Primeiro movimente o servo da base");
-
-		do{
-        tecla = getch();
-        if(tecla == 224){
-            tecla = getch();
-                if(tecla == INCREMENTAR){
-                   base = base + KEY_RIGHT;
-                }
-                else if(tecla == REDUZIR){
-                   base = base - KEY_LEFT;
-                }
-                else{
-                   printf("Você apertou uma tecla inválida.\nTente novamente");
-                }
-         }
-         else if(tecla != 122){
-            printf("Você apertou uma tecla inválida.\nTente novamente");
-         }
-         if(base<=BAS_MIN){
-            printf("Você atingiu o limite mínimo de movimentação");
-            break;
-         }
-         if(base>=BAS_MAX){
-            printf("Você atingiu o limite máximo de movimentação");
-            break;
-         }
-         sprintf(comando,"#%dP%d",BAS_SERVO,base);
-		 enviar_comando(comando,serial_fd);
-		 memset(comando, 0, BUFSIZE);
-         }while(tecla!=122);
-
-        printf("Agora vamos movimentar o servo do ombro");
-
-		do{
-        tecla = getch();
-        if(tecla == 224){
-            tecla = getch();
-                if(tecla == INCREMENTAR){
-                   ombro = ombro + KEY_RIGHT;
-                }
-                else if(tecla == REDUZIR){
-                   ombro = ombro - KEY_LEFT;
-                }
-                else{
-                   printf("Você apertou uma tecla inválida.\nTente novamente");
-                }
-         }
-         else if(tecla != 122){
-            printf("Você apertou uma tecla inválida.\nTente novamente");
-         }
-         if(base<=SHL_MIN){
-            printf("Você atingiu o limite mínimo de movimentação");
-            break;
-         }
-         if(base>=SHL_MAX){
-            printf("Você atingiu o limite máximo de movimentação");
-            break;
-         }
-         sprintf(comando,"#%dP%d",SHL_SERVO,base);
-		 enviar_comando(comando,serial_fd);
-		 memset(comando, 0, BUFSIZE);
-         }while(tecla!=122);
-
-         printf("Movimentando agora o cotovelo");
-
-		do{
-        tecla = getch();
-        if(tecla == 224){
-            tecla = getch();
-                if(tecla == INCREMENTAR){
-                   cotovelo = cotovelo + KEY_RIGHT;
-                }
-                else if(tecla == REDUZIR){
-                   cotovelo = cotovelo - KEY_LEFT;
-                }
-                else{
-                   printf("Você apertou uma tecla inválida.\nTente novamente");
-                }
-         }
-         else if(tecla != 122){
-            printf("Você apertou uma tecla inválida.\nTente novamente");
-         }
-         if(base<=ELB_MIN){
-            printf("Você atingiu o limite mínimo de movimentação");
-            break;
-         }
-         if(base>=ELB_MAX){
-            printf("Você atingiu o limite máximo de movimentação");
-            break;
-         }
-         sprintf(comando,"#%dP%d",ELB_SERVO,base);
-		 enviar_comando(comando,serial_fd);
-		 memset(comando, 0, BUFSIZE);
-         }while(tecla!=122);
-
-         printf("Punho...");
-
-		do{
-        tecla = getch();
-        if(tecla == 224){
-            tecla = getch();
-                if(tecla == INCREMENTAR){
-                   punho = punho + KEY_RIGHT;
-                }
-                else if(tecla == REDUZIR){
-                   punho = punho - KEY_LEFT;
-                }
-                else{
-                   printf("Você apertou uma tecla inválida.\nTente novamente");
-                }
-         }
-         else if(tecla != 122){
-            printf("Você apertou uma tecla inválida.\nTente novamente");
-         }
-         if(base<=WRI_MIN){
-            printf("Você atingiu o limite mínimo de movimentação");
-            break;
-         }
-         if(base>=WRI_MAX){
-            printf("Você atingiu o limite máximo de movimentação");
-            break;
-         }
-         sprintf(comando,"#%dP%d",WRI_SERVO,base);
-		 enviar_comando(comando,serial_fd);
-		 memset(comando, 0, BUFSIZE);
-         }while(tecla!=122);
-
-         printf("E finalmente a garra");
-
-		do{
-        tecla = getch();
-        if(tecla == 224){
-            tecla = getch();
-                if(tecla == INCREMENTAR){
-                   garra = garra + KEY_RIGHT;
-                }
-                else if(tecla == REDUZIR){
-                   garra = garra - KEY_LEFT;
-                }
-                else{
-                   printf("Você apertou uma tecla inválida.\nTente novamente");
-                }
-         }
-         else if(tecla != 122){
-            printf("Você apertou uma tecla inválida.\nTente novamente");
-         }
-         if(base<=GRI_MIN){
-            printf("Você atingiu o limite mínimo de movimentação");
-            break;
-         }
-         if(base>=GRI_MAX){
-            printf("Você atingiu o limite máximo de movimentação");
-            break;
-         }
-         sprintf(comando,"#%dP%d",GRI_SERVO,base);
-		 enviar_comando(comando,serial_fd);
-		 memset(comando, 0, BUFSIZE);
-         }while(tecla!=122);
-
-         fechar_porta(serial_fd);
-	}
+x = cos(A1)*(cos(A2+A3+A4)*5.7+cos(A2+A3)*18.6+cos(A2)*14.6);
 
 
-   return 0;
+
+return x;
+
+
 
 }
 
+double cinedireta2(base,ombro,cotovelo,punho)
+
+{
+
+double y, A1,A2,A3,A4;
+
+A1 = ((base-500)*0.09 + 180)*(PI/180);
+
+A2 = ((ombro-500)*0.09)*(PI/180);
+
+A3 = ((cotovelo-500)*0.09)*(PI/180);
+
+A4 = ((punho-500)*0.09 - 90)*(PI/180);
+
+y = sin(A1)*(cos(A2+A3+A4)*5.7+cos(A2+A3)*18.6+cos(A2)*14.6 );
+
+
+
+return y;
+
+
+
+}
+
+double cinedireta3(base,ombro,cotovelo,punho)
+
+{
+
+double z, A1,A2,A3,A4;
+
+A1 = ((base-500)*0.09 + 180)*(PI/180);
+
+A2 = ((ombro-500)*0.09)*(PI/180);
+
+A3 = ((cotovelo-500)*0.09)*(PI/180);
+
+A4 = ((punho-500)*0.09 - 90)*(PI/180);
+
+z = sin(A2+A3+A4)*5.7 + sin(A2+A3)*18.6 + sin(A2)*14.6 + 6.6;
+
+return z;
+
+
+
+}
+int main()
+{
+    double r1,r2,r3;
+    int base = 1500, ombro = 2000, cotovelo = 1500, punho = 2500, garra = 1500;
+     r1 = cinedireta1(base,ombro,cotovelo,punho);
+
+        r2 = cinedireta2(base,ombro,cotovelo,punho);
+
+        r3 = cinedireta3(base,ombro,cotovelo,punho);
+
+        printf("valor x: %.3f ",r1);
+
+        printf("valor y: %.3f ",r2);
+
+        printf("valor z: %.3f ",r3);
+}
