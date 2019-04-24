c0592a2985d66ba374b1bf81154bd1e86d2be78f
vna15 <30414490+vna15@users.noreply.github.com>
2019-04-24 14:54:18 -0300

    Update CLinuxCerto.c

--- CLinuxCerto.c
+++ CLinuxCerto.c
@@ -12,9 +12,9 @@ double cinedireta1(base,ombro,cotovelo,punho)
 {
 double x, A1,A2,A3,A4;
 A1 = ((base-500)*0.09 + 180)*(PI/180);
-A2 = ((ombro-500)*0.09)*(PI/180);
+A2 = -((ombro-500)*0.09 + 180)*(PI/180);
 A3 = ((cotovelo-500)*0.09)*(PI/180);
-A4 = ((punho-500)*0.09 - 90)*(PI/180);
+A4 = -((punho-500)*0.09 - 90)*(PI/180);
 x = cos(A1)*(cos(A2+A3+A4)*5.7+cos(A2+A3)*18.6+cos(A2)*14.6);
 
 return x;
@@ -24,9 +24,9 @@ double cinedireta2(base,ombro,cotovelo,punho)
 {
 double y, A1,A2,A3,A4;
 A1 = ((base-500)*0.09 + 180)*(PI/180);
-A2 = ((ombro-500)*0.09)*(PI/180);
+A2 = -((ombro-500)*0.09 + 180)*(PI/180);
 A3 = ((cotovelo-500)*0.09)*(PI/180);
-A4 = ((punho-500)*0.09 - 90)*(PI/180);
+A4 = -((punho-500)*0.09 - 90)*(PI/180);
 y = sin(A1)*(cos(A2+A3+A4)*5.7+cos(A2+A3)*18.6+cos(A2)*14.6);
 
 return y;
@@ -36,9 +36,9 @@ double cinedireta3(base,ombro,cotovelo,punho)
 {
 double z, A1,A2,A3,A4;
 A1 = ((base-500)*0.09 + 180)*(PI/180);
-A2 = ((ombro-500)*0.09)*(PI/180);
+A2 = -((ombro-500)*0.09 + 180)*(PI/180);
 A3 = ((cotovelo-500)*0.09)*(PI/180);
-A4 = ((punho-500)*0.09 - 90)*(PI/180);
+A4 = -((punho-500)*0.09 - 90)*(PI/180);
 z = sin(A2+A3+A4)*5.7 + sin(A2+A3)*18.6 + sin(A2)*14.6 + 6.6;
 return z;
 
@@ -216,6 +216,7 @@ double r1,r2,r3;
         }
         while(n_servo!=5);
         fechar_porta(serial_fd);
+
         r1 = cinedireta1(base,ombro,cotovelo,punho);
         r2 = cinedireta2(base,ombro,cotovelo,punho);
         r3 = cinedireta3(base,ombro,cotovelo,punho);
